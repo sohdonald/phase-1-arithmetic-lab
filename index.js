3e0cf3a70eafedc6cfd096feef62560717f895f5
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-03-31 21:49:48 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+//finally got part 1 solved
+
+const random = Math.floor(Math.random() * 10) +1;
+
+// 2 pass, 2 to go
+
+const num3 = 19;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+//3 pass
+
+const max = Math.max(3,1,15,20,8);
